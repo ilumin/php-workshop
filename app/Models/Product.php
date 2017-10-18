6187f319efaf7fc595478eb60183238d5ede1188
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-10-18 22:34:28 +0700

    UPDATE product model
    (cherry picked from commit 85f48da)

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed        name
+ * @property mixed        detail
+ * @property mixed        price
+ * @property false|string thumbnail
+ */
 class Product extends Model
 {
-    public $name;
-    public $price;
-    public $detail;
-    public $thumbnail;
-
     protected $fillable = ['name', 'price', 'detail', 'thumbnail'];
 }
