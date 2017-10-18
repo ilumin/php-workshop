37dd5483124af12bb72a2103a66e37cd13a930d2
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-10-18 21:52:03 +0700

    UPDATE product migrate
    (cherry picked from commit a2bac5e)

--- database/migrations/2017_10_18_144554_create_products_table.php
+++ database/migrations/2017_10_18_144554_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->float('price');
+            $table->string('thumbnail');
+            $table->text('detail');
             $table->timestamps();
         });
     }
