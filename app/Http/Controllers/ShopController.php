8af0d7fa53400ae80f046c8da253c5c54e9c1c42
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-10-18 23:58:26 +0700

    ADD display logic

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     public function listProduct(Request $request)
     {
-        return view('shop.list-product');
+        $products = Product::orderBy('updated_at', 'desc')->paginate(8);
+        return view('shop.list-product', [
+            'products' => $products
+        ]);
     }
 }
--- resources/views/shop/list-product.blade.php
+++ resources/views/shop/list-product.blade.php
@@ -6,15 +6,16 @@
     <div class="container">
         <div class="row">
 
+            @foreach($products as $product)
             <div class="col-sm-6 col-md-4">
-                <a href="#" class="thumbnail">
-                    <img src="thumbnail" alt="name">
+                <a href="{{ url('/product/' . $product->id) }}" class="thumbnail">
+                    <img src="{{ Storage::url($product->thumbnail) }}" alt="{{ $product->name }}">
                     <div class="caption">
-                        <h3>name</h3>
-                        <p>detail</p>
-                    </div>
+                        <h3>{{ $product->name }}</h3>
+                     /div>
                 </a>
             </div>
+            @endforeach
 
         </div>
     </div>
