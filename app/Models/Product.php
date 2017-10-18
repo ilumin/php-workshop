df58e6fc9063a3b6fd1e7f3159de538d3866b35d
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-10-18 21:58:45 +0700

    UPDATE model property
    (cherry picked from commit 00f54a7)

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public $name;
+    public $price;
+    public $detail;
+    public $thumbnail;
+
+    protected $fillable = ['name', 'price', 'detail', 'thumbnail'];
 }
