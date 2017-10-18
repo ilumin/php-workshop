28abcfd38f20b5deb0e338114191e9411066e6bc
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-10-19 00:48:46 +0700

    UPDATE model data and relation
    (cherry picked from commit b40bf9f)

--- app/Models/ShoppingCart.php
+++ app/Models/ShoppingCart.php
@@ -4,7 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed        total
+ * @property mixed        status
+ */
 class ShoppingCart extends Model
 {
-    //
+    protected $fillable = ['total', 'status'];
+
+    public function items()
+    {
+        return $this->hasMany('App\Models\ShoppingCartItem');
+    }
 }
--- app/Models/ShoppingCartItem.php
+++ app/Models/ShoppingCartItem.php
@@ -4,7 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed        name
+ * @property mixed        detail
+ * @property mixed        price
+ * @property false|string thumbnail
+ */
 class ShoppingCartItem extends Model
 {
-    //
+    protected $fillable = ['shopping_cart_id', 'product_id', 'price', 'qty', 'total'];
+
+    public function product()
+    {
+        return $this->hasOne('App\Models\Product');
+    }
 }
